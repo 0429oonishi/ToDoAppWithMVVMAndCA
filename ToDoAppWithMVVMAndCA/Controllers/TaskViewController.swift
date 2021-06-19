2c62901ff36495e77efd54fc0fd072a887ecc600
0429oonishi <oonishireon110@gmail.com>
2021-06-19 22:38:59 +0900

    checkButtonを押したときに画像を反転

--- ToDoAppWithMVVMAndCA/Controllers/TaskViewController.swift
+++ ToDoAppWithMVVMAndCA/Controllers/TaskViewController.swift
@@ -10,17 +10,55 @@ import UIKit
 final class TaskViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
+    private var testData = Task.testData
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        setupTableView()
         
     }
 
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(TaskTableViewCell.nib,
+                           forCellReuseIdentifier: TaskTableViewCell.identifier)
+    }
+    
     @IBAction private func addTaskButtonDidTapped(_ sender: Any) {
         
     }
     
 }
 
+extension TaskViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+}
+
+extension TaskViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        testData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TaskTableViewCell.identifier,
+                                                 for: indexPath) as! TaskTableViewCell
+        let task = testData[indexPath.row]
+        cell.configure(task: task) { [weak self] in
+            self?.testData[indexPath.row].isChecked.toggle()
+            DispatchQueue.main.async {
+                self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
+        return cell
+    }
+    
+    
+}
--- ToDoAppWithMVVMAndCA/Views/Cells/TaskTableViewCell.swift
+++ ToDoAppWithMVVMAndCA/Views/Cells/TaskTableViewCell.swift
@@ -12,8 +12,28 @@ final class TaskTableViewCell: UITableViewCell {
     @IBOutlet private weak var checkButton: UIButton!
     @IBOutlet private weak var taskLabel: UILabel!
     
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    var checkButtonDidTapped: (() -> Void)?
+    
     @IBAction private func checkButtonDidTapped(_ sender: Any) {
+        checkButtonDidTapped?()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
         
+        checkButton.setImage(UIImage(systemName: "circle"), for: .normal)
+        
+    }
+    
+    func configure(task: Task, checkButtonDidTapped: (() -> Void)?) {
+        taskLabel.text = task.title
+        checkButton.setImage(
+            UIImage(systemName: { task.isChecked ? "circle.fill" : "circle" }()),
+            for: .normal
+        )
+        self.checkButtonDidTapped = checkButtonDidTapped
     }
     
 }
