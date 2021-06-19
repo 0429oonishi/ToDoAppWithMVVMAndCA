5fb3ef78447bfd7bb4976b27f52c59f6f2c4e8af
0429oonishi <oonishireon110@gmail.com>
2021-06-19 22:04:42 +0900

    VC->TaskVC

---
+++ ToDoAppWithMVVMAndCA/TaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TaskViewController.swift
+//  ToDoAppWithMVVMAndCA
+//
+//  Created by 大西玲音 on 2021/06/19.
+//
+
+import UIKit
+
+final class TaskViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    @IBAction private func addTaskButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+}
+
