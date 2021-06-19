686b4020e576c9d1f2d41e7e0122990dfcad5e4b
0429oonishi <oonishireon110@gmail.com>
2021-06-19 22:38:27 +0900

    Task実装

--- ToDoAppWithMVVMAndCA/Models/Task.swift
+++ ToDoAppWithMVVMAndCA/Models/Task.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Task {
+    var title: String
+    var isChecked: Bool
+    static let testData = [Task(title: "AAA", isChecked: false),
+                           Task(title: "BBB", isChecked: false),
+                           Task(title: "CCC", isChecked: false),
+                           Task(title: "DDD", isChecked: false),
+                           Task(title: "EEE", isChecked: false),
+    ]
+}
