874ae4d3db9498583afe74c71828b270f371e726
0429oonishi <oonishireon110@gmail.com>
2021-06-19 22:10:38 +0900

    Cell作成

---
+++ ToDoAppWithMVVMAndCA/Views/Cells/TaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskTableViewCell.swift
+//  ToDoAppWithMVVMAndCA
+//
+//  Created by 大西玲音 on 2021/06/19.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
