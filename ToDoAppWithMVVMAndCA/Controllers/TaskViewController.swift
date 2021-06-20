2daab0ae931342768760071daed842eb63921fbb
0429oonishi <oonishireon110@gmail.com>
2021-06-20 17:00:09 +0900

    TaskVCをRealmに置き換え

--- ToDoAppWithMVVMAndCA/Controllers/TaskViewController.swift
+++ ToDoAppWithMVVMAndCA/Controllers/TaskViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 final class TaskViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
-    private var testData = Task.testData
+    private var taskUseCase = TaskUseCase(TaskRepositoryImpl())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,15 +39,17 @@ extension TaskViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let editTaskVC = EditTaskViewController.instantiate(text: testData[indexPath.row].title)
+        let editTaskVC = EditTaskViewController.instantiate(
+            text: taskUseCase.read(at: indexPath.row).title
+        )
         editTaskVC.editEvent = { [weak self] text in
-            self?.testData[indexPath.row].title = text
+            self?.taskUseCase.update(text, at: indexPath.row)
             DispatchQueue.main.async {
                 self?.tableView.reloadRows(at: [indexPath], with: .automatic)
             }
         }
         editTaskVC.deleteEvent = { [weak self] in
-            self?.testData.remove(at: indexPath.row)
+            self?.taskUseCase.delete(at: indexPath.row)
             DispatchQueue.main.async {
                 self?.tableView.reloadData()
             }
@@ -64,7 +66,7 @@ extension TaskViewController: UITableViewDelegate {
 extension TaskViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        testData.count
+        return taskUseCase.count()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -72,9 +74,9 @@ extension TaskViewController: UITableViewDataSource {
             withIdentifier: TaskTableViewCell.identifier,
             for: indexPath
         ) as! TaskTableViewCell
-        let task = testData[indexPath.row]
+        let task = taskUseCase.read(at: indexPath.row)
         cell.configure(task: task) { [weak self] in
-            self?.testData[indexPath.row].isChecked.toggle()
+            self?.taskUseCase.toggleCheck(at: indexPath.row)
             DispatchQueue.main.async {
                 self?.tableView.reloadRows(at: [indexPath], with: .automatic)
             }
@@ -82,13 +84,12 @@ extension TaskViewController: UITableViewDataSource {
         return cell
     }
     
-    
 }
 
 extension TaskViewController: AdditionalTaskViewControllerDelegate {
     
     func addButtonDidTapped(text: String) {
-        testData.append(Task(title: text, isChecked: false))
+        taskUseCase.add(text: text)
         DispatchQueue.main.async {
             self.tableView.reloadData()
         }
